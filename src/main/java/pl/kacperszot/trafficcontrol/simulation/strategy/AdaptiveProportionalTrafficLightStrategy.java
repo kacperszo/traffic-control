0a2f9d74ebaa63acd3bb9c114af25ce0e38c3ae3
Kacper Szot <kacperszot1@gmail.com>
2024-11-19 12:19:28 +0100

    AdaptiveProportionalTrafficLightStrategy and tests

--- src/main/java/pl/kacperszot/trafficcontrol/simulation/strategy/AdaptiveProportionalTrafficLightStrategy.java
+++ src/main/java/pl/kacperszot/trafficcontrol/simulation/strategy/AdaptiveProportionalTrafficLightStrategy.java
@@ -9,30 +9,28 @@ import pl.kacperszot.trafficcontrol.model.trafficlight.TrafficLight;
 import pl.kacperszot.trafficcontrol.model.trafficlight.TrafficLightSignal;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class AdaptiveProportionalTrafficLightStrategy implements TrafficLightStrategy {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private final int minGreenLightTicks;
-    private final int maxGreenLightTicks;
-    private final int transitionStateTicks;
+    private final int minGreenTime;
+    private final int maxGreenTime;
+    private final int transitionTime;
     private final double vehicleWeightFactor;
 
     private boolean inTransitionState = false;
-    private int currentTick = 0;
-    private int currentGreenLightDuration;
+    private int currentStep = 0;
+    private int currentGreenDuration;
 
     public AdaptiveProportionalTrafficLightStrategy(
-            int minGreenLightTicks,
-            int maxGreenLightTicks,
-            int transitionStateTicks,
+            int minGreenTime,
+            int maxGreenTime,
+            int transitionTime,
             double vehicleWeightFactor
     ) {
-        this.minGreenLightTicks = minGreenLightTicks;
-        this.maxGreenLightTicks = maxGreenLightTicks;
-        this.transitionStateTicks = transitionStateTicks;
+        this.minGreenTime = minGreenTime;
+        this.maxGreenTime = maxGreenTime;
+        this.transitionTime = transitionTime;
         this.vehicleWeightFactor = vehicleWeightFactor;
     }
 
@@ -40,60 +38,66 @@ public class AdaptiveProportionalTrafficLightStrategy implements TrafficLightStr
     public void setup(Intersection intersection) {
         // Initially set NORTH-SOUTH to GREEN and EAST-WEST to RED
         for (Road road : intersection.getRoads()) {
-            if (road.getDirection() == RoadDirection.NORTH || road.getDirection() == RoadDirection.SOUTH) {
+            if (road.getDirection() == RoadDirection.NORTH ||
+                    road.getDirection() == RoadDirection.SOUTH) {
                 road.getTrafficLight().setState(TrafficLightSignal.GREEN);
             } else {
                 road.getTrafficLight().setState(TrafficLightSignal.RED);
             }
         }
-        // Calculate initial green light duration based on waiting vehicles
-        calculateNextGreenLightDuration(intersection);
+        // Calculate initial green duration based on waiting vehicles
+        // Almost always will be = min time since, we add vehicles after simulation is set up
+        currentGreenDuration = calculateGreenDuration(intersection);
     }
 
     @Override
     public void step(Intersection intersection) {
         List<TrafficLight> trafficLights = getAllTrafficLightsFromIntersection(intersection);
 
-        if (currentTick == currentGreenLightDuration && !inTransitionState) {
-            // Change from GREEN/RED to YELLOW/RED
+        if (currentStep >= currentGreenDuration && !inTransitionState) {
+            // Start transition phase (GREEN -> YELLOW or RED -> RED_YELLOW)
             trafficLights.forEach(TrafficLight::toggleNextState);
-            LOGGER.info("Changing traffic lights to transition state");
+            LOGGER.info("Starting transition phase");
             inTransitionState = true;
-            currentTick = 0;
-        } else if (currentTick == transitionStateTicks && inTransitionState) {
-            // Change from YELLOW/RED_YELLOW to RED/GREEN
+            currentStep = 0;
+        } else if (currentStep >= transitionTime && inTransitionState) {
+            // Complete transition (YELLOW -> RED or RED_YELLOW -> GREEN)
             trafficLights.forEach(TrafficLight::toggleNextState);
-            LOGGER.info("Completing traffic light transition");
+            LOGGER.info("Completing transition phase");
             inTransitionState = false;
-            currentTick = 0;
-            // Calculate next green light duration based on waiting vehicles
-            calculateNextGreenLightDuration(intersection);
+            currentStep = 0;
+            // Calculate new green duration for the next phase
+            currentGreenDuration = calculateGreenDuration(intersection);
         } else {
-            currentTick++;
+            currentStep++;
         }
     }
 
-    private void calculateNextGreenLightDuration(Intersection intersection) {
-        // Get total waiting vehicles for current red direction
-        int waitingVehicles = getTotalWaitingVehiclesForCurrentRed(intersection);
+    private int calculateGreenDuration(Intersection intersection) {
+        int totalWaitingVehicles = 0;
+        TrafficLightSignal currentState = null;
 
-        // Calculate green light duration based on waiting vehicles
-        int adaptiveDuration = (int) (minGreenLightTicks + (waitingVehicles * vehicleWeightFactor));
-        currentGreenLightDuration = Math.min(adaptiveDuration, maxGreenLightTicks);
+        // Count waiting vehicles for the direction that's about to get green light
+        for (Road road : intersection.getRoads()) {
+            TrafficLightSignal state = road.getTrafficLight().getState();
+            if (currentState == null) {
+                currentState = state;
+            }
 
-        LOGGER.info("Calculated green light duration: {} ticks for {} waiting vehicles",
-                currentGreenLightDuration, waitingVehicles);
-    }
+            if ((state == TrafficLightSignal.RED &&
+                    currentState == TrafficLightSignal.RED) ||
+                    (state == TrafficLightSignal.GREEN &&
+                            currentState == TrafficLightSignal.GREEN)) {
+                totalWaitingVehicles += road.getWaitingVehicleCount();
+            }
+        }
 
-    private int getTotalWaitingVehiclesForCurrentRed(Intersection intersection) {
-        return intersection.getRoads().stream()
-                .filter(road -> {
-                    TrafficLightSignal state = road.getTrafficLight().getState();
-                    return state == TrafficLightSignal.RED ||
-                            state == TrafficLightSignal.RED_YELLOW;
-                })
-                .mapToInt(Road::getWaitingVehicleCount)
-                .sum();
+
+        int additionalTime = (int) (totalWaitingVehicles * vehicleWeightFactor);
+        int calculatedDuration = minGreenTime + additionalTime;
+
+
+        return Math.min(calculatedDuration, maxGreenTime);
     }
 
     private List<TrafficLight> getAllTrafficLightsFromIntersection(Intersection intersection) {
